1ed0523177a7497fc6628a249159c00e4b7c00ff
N00240181 <N00240181@iadt.ie>
2026-02-16 14:24:16 +0000

    Created health and a damage health function to test how health would work and what a game over screen could be like.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,32 @@
 let score = 0;
+let health = 100;
+let bgColor = "cyan"
 
 function incrementScore() {
     score += 1;
     return score;
 }
 
+function damageHealth() {
+    health -= 25
+    if (health <= 0) {
+        bgColor = "black";
+        fill("red");
+        textSize(36)
+        text("Finished", 10, 80)
+        noLoop();
+    }
+}
+
 function setup() {
     createCanvas(640, 480)
-
     setInterval(incrementScore, 10)
+    setInterval(damageHealth, 1000)
 }
 
 function draw() {
-    background("cyan")
+    background(bgColor)
+    textSize(12)
     text(`Score: ${score}`, 10, 20)
+    text(`Health: ${health}`, 10, 40)
 }
