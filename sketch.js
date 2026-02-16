95cd9d9c37864ffd46bf975639a0b6bb0035bd46
N00240181 <N00240181@iadt.ie>
2026-02-16 14:40:35 +0000

    Added ability to move player around the canvas at a speed defined at the top.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let score = 0;
 let health = 100;
-let bgColor = "cyan"
+let bgColor = "cyan";
+let playerX = 10;
+let playerY = 100;
+let playerSpeed = 5;
 
 function preload() {
     playerImg = loadImage('/images/player/player.gif');
@@ -12,30 +15,46 @@ function incrementScore() {
 }
 
 function spawnPlayer() {
-    image(playerImg, 10, 100, 100, 100)
+    image(playerImg, playerX, playerY, 100, 100);
 }
 
 function damageHealth() {
-    health -= 1
+    health -= 1;
     if (health <= 0) {
         bgColor = "black";
         fill("red");
-        textSize(36)
-        text("Finished", 10, 80)
+        textSize(36);
+        text("Finished", 10, 80);
         noLoop();
     }
 }
 
+function movePlayer() {
+    if(keyIsDown(UP_ARROW)) {
+        playerY -= playerSpeed;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+        playerY += playerSpeed;
+    }
+    if(keyIsDown(LEFT_ARROW)) {
+        playerX -= playerSpeed;
+    }
+    if(keyIsDown(RIGHT_ARROW)) {
+        playerX += playerSpeed;
+    }
+}
+
 function setup() {
-    createCanvas(640, 480)
-    setInterval(incrementScore, 10)
+    createCanvas(640, 480);
+    setInterval(incrementScore, 10);
     /* setInterval(damageHealth, 40) */
 }
 
 function draw() {
-    background(bgColor)
-    textSize(12)
-    text(`Score: ${score}`, 10, 20)
-    text(`Health: ${health}`, 10, 40)
-    spawnPlayer()
+    background(bgColor);
+    textSize(12);
+    text(`Score: ${score}`, 10, 20);
+    text(`Health: ${health}`, 10, 40);
+    spawnPlayer();
+    movePlayer();
 }
