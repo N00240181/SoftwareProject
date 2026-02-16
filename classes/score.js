2b25902155236eb8b91b7487ee5a3f482df70407
N00240181 <N00240181@iadt.ie>
2026-02-16 14:16:10 +0000

    Created a scoring function in the sketch file to test how it works.

---
+++ classes/score.js
@@ -0,0 +1,10 @@
+/* let score = null;
+
+function incrementScore() {
+    for(let i = 0; i > 0; i++) {
+        setTimeout(1000);
+        score += 100;
+        console.log(score)
+    }
+    
+} */
--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let score = 0;
+
+function incrementScore() {
+    score += 1;
+    return score;
+}
+
 function setup() {
     createCanvas(640, 480)
+
+    setInterval(incrementScore, 10)
 }
 
 function draw() {
     background("cyan")
+    text(`Score: ${score}`, 10, 20)
 }
