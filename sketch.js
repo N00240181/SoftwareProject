d51b5e95cb9518988fe1c23ae12a7166918b4e67
N00240181 <N00240181@iadt.ie>
2026-02-20 12:04:04 +0000

    Added fish and fishy images

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 let score = 0;
 let health = 100;
 let bgColor = "cyan";
+let player;
 let playerX = 10;
 let playerY = 100;
 let playerSpeed = 5;
+let fishes = [];
+let fishX;
+let fishY;
 let items = [];
 let itemX;
 let itemY;
-let randImage;
+let randItemImage;
+let randFishImage;
 
 function preload() {
-    playerImg = loadImage('/images/player/player.gif');
+    playerImg = loadImage('images/player/player.gif');
     bgImg = loadImage('/images/background/background.png')
     itemImages = [
         loadImage('/images/bags/blackbag.png'),
@@ -34,7 +39,18 @@ function preload() {
         loadImage('/images/glass/glass1.png'),
 
         loadImage('/images/packaging/closedBox.png'),
-        loadImage('/images/packaging/kelpo.png')
+        loadImage('/images/packaging/kelpo.png'),
+
+        loadImage('/images/skeleton.gif')
+    ];
+    fishImages = [
+        loadImage('images/fish/fish1.png'),
+        loadImage('images/fish/fish2.png'),
+        loadImage('images/fish/fish3.png'),
+        loadImage('images/fish/fish4.png'),
+        loadImage('images/fish/fish5.png'),
+        loadImage('images/fish/fish6.png'),
+        loadImage('images/fish/fish7.png')
     ]
 }
 
@@ -44,7 +60,7 @@ function incrementScore() {
 }
 
 function spawnPlayer() {
-    image(playerImg, playerX, playerY, 100, 100);
+    image(playerImg, playerX, playerY, 20, 20);
 }
 
 function damageHealth() {
@@ -71,12 +87,17 @@ function movePlayer() {
     if(keyIsDown(RIGHT_ARROW)) {
         playerX += playerSpeed;
     }
+    if(dist([playerX, playerY, items.itemX, items.itemY]) <= 50) {
+        console.log("jogn")
+    }
 }
 
 function setup() {
     createCanvas(640, 480);
+    frameRate(60);
     setInterval(incrementScore, 10);
-    setInterval(spawnItems, 2000)
+    setInterval(spawnItems, 1000);
+    setInterval(spawnFish, 1000);
     /* setInterval(damageHealth, 120) */
 }
 
@@ -89,24 +110,38 @@ function draw() {
     text(`Health: ${health}`, 10, 40);
     spawnPlayer();
     movePlayer();
-    for (let i = 0; i < items.length; i++) {
-        image(items[i].img, items[i].itemX, items[i].itemY, 100, 100);
-        while(items[i].itemX > 0) {
-            items[i].itemX -= 20
-        }
-    }
-}
+    moveItems()
+    moveFishes();
+};
 
 function spawnItems() {
     let item = {
-        itemX: 540,
-        itemY: Math.floor(Math.random() * 380),
+        itemX: 690,
+        itemY: Math.floor(Math.random() * 420),
         img: itemImages[Math.floor(random(itemImages.length))]
     }
     items.push(item);
-    /* let itemX = 540
-    let itemY = Math.floor(Math.random() * 440)
-    let randImage = Math.floor(random(itemImages.length))
-    let itemImage = loadImage(itemImages[randImage])
-    image(itemImage, itemX, itemY, 100, 100); */
+}
+
+function spawnFish() {
+    let fish = {
+        fishX: 690,
+        fishY: Math.floor(Math.random() * 420),
+        img: fishImages[Math.floor(random(fishImages.length))]
+    }
+    fishes.push(fish);
+}
+
+function moveItems() {
+    for (let i = 0; i < items.length; i++) {
+        image(items[i].img, items[i].itemX, items[i].itemY, 100, 100);
+        items[i].itemX -= 1
+    }
+}
+
+function moveFishes() {
+    for (let i = 0; i < fishes.length; i++) {
+        image(fishes[i].img, fishes[i].fishX, fishes[i].fishY, 100, 100);
+        fishes[i].fishX -= 1
+    }
 }
